8d42a5376de848d46ff369a69a032345633fb27b
Shobha-g89 <117902173+Shobha-g89@users.noreply.github.com>
2024-04-25 13:37:24 +0530

    Initial commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Shobha\OneDrive\Desktop\SchoolManagement\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
